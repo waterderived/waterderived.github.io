ee45c67657b66c6a67bbfb68b0b2f8ae527f9ba6
waterderived <nickjisookim@gmail.com>
2024-04-02 01:31:49 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,49 +1,51 @@
 var dropdown = document.getElementsByClassName("sidebar-button-dropdown");
-var i;
-        
-for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
-            if (dropdownContent.style.display === "block") {
-                dropdownContent.style.display = "none";
-            } 
-            else {
-                dropdownContent.style.display = "block";
-            }
-        });
-    }
+        var i;
 
-//sidebar collapse
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-            function openNav() {
-                document.getElementById("mySidebar").style.width = "110px";
-                document.getElementById("main").style.marginLeft = "110px";
-            }
+        for (i = 0; i < dropdown.length; i++) {
+            dropdown[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                var dropdownContent = this.nextElementSibling;
+                if (dropdownContent.style.display === "block") {
+                    dropdownContent.style.display = "none";
+                }
+                else {
+                    dropdownContent.style.display = "block";
+                }
+            });
+        }
 
-            //hide sidebar button
-            const div = document.querySelector('.sidebar-open-button')
+        //sidebar collapse
+        /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+        function openNav() {
+            document.getElementById("mySidebar").style.width = "110px";
+            document.getElementById("main").style.marginLeft = "0";
+        }
 
-            div.addEventListener('click', () => {
-                div.classList.add('hidden')
-            })
+        //hide sidebar button
+        const div = document.querySelector('.sidebar-open-button')
 
-            /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-            function closeNav() {
-              document.getElementById("mySidebar").style.width = "0";
-              document.getElementById("main").style.marginLeft = "0";
-              div.classList.remove('hidden'); //reappear sidebar button
-            }
+        div.addEventListener('click', () => {
+            div.classList.add('hidden');
+        })
 
-            //theme switcher
-            const switchTheme = () => {
-                const rootElem = document.documentElement
-                let dataTheme = rootElem.getAttribute('data-theme'),
-                    newTheme
 
-                newTheme = (dataTheme === 'light') ? 'dark' : 'light'
+        /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+        function closeNav() {
+            document.getElementById("mySidebar").style.width = "0";
+            document.getElementById("main").style.marginLeft = "0";
+            div.classList.remove('hidden'); //reappear sidebar button
+        }
 
-                rootElem.setAttribute('data-theme', newTheme)
-            }
 
-            document.querySelector('#theme-switcher').addEventListener('click', switchTheme)
+        //theme switcher
+        const switchTheme = () => {
+            const rootElem = document.documentElement
+            let dataTheme = rootElem.getAttribute('data-theme'),
+                newTheme
+
+            newTheme = (dataTheme === 'light') ? 'dark' : 'light'
+
+            rootElem.setAttribute('data-theme', newTheme)
+        }
+
+        document.querySelector('#theme-switcher').addEventListener('click', switchTheme)
