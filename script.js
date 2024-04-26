865256abea7b90419342f6b22f81da08642be9d1
waterderived <nickjisookim@gmail.com>
2024-04-26 14:41:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var dropdown = document.getElementsByClassName("sidebar-button-dropdown");
+/* var dropdown = document.getElementsByClassName("sidebar-button-dropdown");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
@@ -13,11 +13,11 @@ for (i = 0; i < dropdown.length; i++) {
             dropdownContent.style.display = "block";
         }
     });
-}
+}*/
 
 //sidebar collapse
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
+/*function openNav() {
     document.getElementById("mySidebar").style.width = "110px";
     document.getElementById("main").style.marginLeft = "0";
 }
@@ -27,17 +27,16 @@ const div = document.querySelector('.sidebar-open-button')
 
 div.addEventListener('click', () => {
     div.classList.add('hidden');
-})
+})*/
 
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
+/*function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     div.classList.remove('hidden'); //reappear sidebar button
-}
+}*/
 
-// evaluate
 function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
     if (localStorageTheme !== null) {
         return localStorageTheme;
